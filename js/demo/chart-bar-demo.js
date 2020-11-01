bcf78ca82f3d9ad57d43f4d03d706daed81b5bd6
Diogo Ramalho <diogo.ramalho1998@gmail.com>
2020-11-01 09:32:52 +0000

    Adds form initial page

--- js/demo/chart-bar-demo.js
+++ js/demo/chart-bar-demo.js
@@ -32,13 +32,13 @@ var ctx = document.getElementById("myBarChart");
 var myBarChart = new Chart(ctx, {
   type: 'horizontalBar',
   data: {
-    labels: ["Hosp1", "Hosp2", "Hosp3", "Hosp4"],
+    labels: ["H. São João", "H. Braga", "H. Santa Maria", "H. Faro"],
     datasets: [{
       label: "Pontos",
       backgroundColor: "#4e73df",
       hoverBackgroundColor: "#2e59d9",
       borderColor: "#4e73df",
-      data: [16,14,18,15],
+      data: [20,18,16,15],
     }],
   },
   options: {
